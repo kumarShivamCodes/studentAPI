b0dfbd4d02959377a153ae3e3350031efa049b6c
shivam kumar <rajput.shivam75298@gmail.com>
2024-08-25 23:54:05 +0530

    adding appilcation insights and openapi decorations

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using studentAPI.Service;
 using studentAPI.Models;
-using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace studentAPI.Controllers;
 
@@ -18,14 +18,29 @@ public class StudentController: ControllerBase
     _logger = logger;
   }
 
+  /// <summary>
+  /// Retrieves a list of all students
+  /// </summary>
+  /// <returns>List of students</returns>
   [HttpGet]
+  [SwaggerResponse(StatusCodes.Status200OK,Type = typeof(IEnumerable<Student>))]
+  [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+  [Produces("application/json")]
   public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
   {
     _logger.LogInformation("Fetching all students");
     return Ok( await _studentService.GetStudentsAsync());
   } 
 
-  [HttpGet("{id}")]
+  /// <summary>
+  /// Retrieves a specific student by ID.
+  /// </summary>
+  /// <param name="id">ID of the student </param>
+  /// <returns>Student with specified id  </returns>
+  [HttpGet("{id:int}")]
+  [SwaggerResponse(StatusCodes.Status200OK)]
+  [SwaggerResponse(StatusCodes.Status404NotFound)]
+  [Produces("application/json")]
   public async Task<ActionResult<Student>> GetStudent(int id)
   {
    _logger.LogInformation("Get Student By Id request"); 
@@ -33,7 +48,15 @@ public class StudentController: ControllerBase
    return Ok(student);
   }
 
+  /// <summary>
+  /// Creates a new a student with the provided data
+  /// </summary>
+  /// <returns>Created student object</returns>
   [HttpPost]
+  [SwaggerResponse(StatusCodes.Status201Created)]
+  [SwaggerResponse(StatusCodes.Status400BadRequest)]
+  [Produces("application/json")]
+  [Consumes("application/json")]
   public async Task<ActionResult<Student>> PostStudent(Student student)
   {
     _logger.LogInformation("Create Student request");
@@ -46,7 +69,18 @@ public class StudentController: ControllerBase
             return CreatedAtAction(nameof(GetStudent), new { id = createdStudent.Id }, createdStudent);
   }
 
-  [HttpPut("{id}")]
+  /// <summary>
+  /// Updates an existing student.
+  /// </summary>
+  /// <param name="id">Student ID</param>
+  /// <param name="student">Updated student object</param>
+  /// <returns>Updated student object</returns>
+  [HttpPut("{id:int}")]
+  [SwaggerResponse(StatusCodes.Status200OK)]
+  [SwaggerResponse(StatusCodes.Status400BadRequest)]
+  [SwaggerResponse(StatusCodes.Status404NotFound)]
+  [Produces("application/json")]
+  [Consumes("application/json")]
   public async Task<ActionResult<Student>> PutStudent(int id, Student student)
   {
     _logger.LogInformation("Update Student request");
@@ -61,7 +95,14 @@ public class StudentController: ControllerBase
     
   }
 
-  [HttpDelete("{id}")]
+  /// <summary>
+  /// Deletes an existing student.
+  /// </summary>
+  /// <param name="id">Student ID</param>
+  /// <returns>No content</returns>
+  [HttpDelete("{id:int}")]
+  [SwaggerResponse(StatusCodes.Status200OK)]
+  [SwaggerResponse(StatusCodes.Status404NotFound)]
   public async Task<ActionResult> DeleteStudent(int id)
   {
     _logger.LogInformation("Delete Student request");
--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
-using Microsoft.EntityFrameworkCore;
 using studentAPI.Service;
 using Azure.Data.Tables;
 using OpenTelemetry.Logs;
-
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // adding loggers
 builder.Logging.ClearProviders();
 builder.Logging.AddOpenTelemetry(x => x.AddConsoleExporter());
+
+// Adding Application Insights for logging
+builder.Logging.AddApplicationInsights(
+    configureTelemetryConfiguration: (config) => 
+        config.ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"],
+    configureApplicationInsightsLoggerOptions: (options) => { }
+);
+// adding application insights for logging
+// builder.Logging.AddApplicationInsights(
+//         configureTelemetryConfiguration: (config) => 
+//             config.ConnectionString = builder.Configuration.GetConnectionString("APPLICATIONINSIGHTS_CONNECTION_STRING"),
+//             configureApplicationInsightsLoggerOptions: (options) => { }
+//     );
 
 // Register your StudentServiceAzure as the implementation of IStudentService
 builder.Services.AddScoped<IStudentService, StudentServiceAzure>();
@@ -30,6 +43,28 @@ builder.Services.AddSingleton(new TableServiceClient("DefaultEndpointsProtocol=h
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Swagger and configure it to use the XML documentation file
+builder.Services.AddSwaggerGen(options =>
+{
+    // Specify the OpenAPI info
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Student API",
+        Description = "An API to manage students",
+        Contact = new OpenApiContact
+        {
+            Name = "Your Name",
+            Email = "your.email@example.com",
+        }
+    });
+
+    // Include XML comments if generated
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
